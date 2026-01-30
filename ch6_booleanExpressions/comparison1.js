cd09feb03c62202a472a7b25125594a3e236d201
carolcroninoide <carol.cronin@oide.ie>
2026-01-30 12:38:50 +0000

    Add files via upload

---
+++ ch6_booleanExpressions/comparison1.js
@@ -0,0 +1,8 @@
+// Comparisons – same datatypes
+console.log(3 < 20); // true
+console.log(7 != 7); // false
+console.log(5 >= 7); // false
+console.log("cat" == "dog"); // false
+console.log("man" != "woman"); // true
+console.log("man" < "woman"); // true
+console.log("man" <= "men"); // true
